0a98ed9baa9e995abc7a32020a7c152d461725d9
Aruisehu <Yohann.JOLAIN@supinfo.com>
2016-05-14 14:46:40 +0200

    Printing the entire piano

--- printing piano.asm
+++ printing piano.asm
@@ -1,19 +1,55 @@
 org 100h 
 
-call print_key
+call print_piano
+mov bx, offset color
+mov [bx], 11111010b ; just to test if ancient piano is well override
+call print_piano
 ret
-print_key PROC
+print_piano PROC
     mov bx, offset column
-    mov dl, [bx] ; load cursor's column
+    mov [bx], 0
+    mov bx, offset key_off
+    mov [bx], 0; put the value to 0
+    mov dx, 0000h
+    mov bh, 0
+    mov ah, 2
+    int 10h ; puts the cursor on the first column, first row
+    mov cx, 8; use as a counter
+    key:
+    mov bx, offset row
+    mov [bx], 0 ; restore the value of the row
+    push cx ; save the value of the counter
+    call print_key
+    pop cx ; restore this value
+    mov bx, offset key_off
+    add [bx], 7 ; use to change key
+    dec cx
+    cmp cx, 0
+    jnz key ;loop
+    ret
+print_piano ENDP
+
+print_key PROC
+    mov bx, offset key_off
+    mov dh, [bx]
+    mov bx, offset column
+    mov [bx], dh
+    mov dl, [bx] ; look which key to print (by looking which offset of key we have)
+    mov cx, 8
     mov bx, offset row
     mov dh, [bx] ; load cursor's row
+    mov bh, 0
+    mov ah, 2
+    int 10h ; move the cursor to print this key
     call print_key_row ; call function
     mov cx, 12 ;use to loop
     etiq:
+    mov bx, offset key_off
+    mov dh, [bx]
     mov bx, offset column
-    mov [bx], 0
+    mov [bx], dh
     mov dl, [bx]
-    mov bx, offset row
+    mov bx, offset row ;restore the value
     inc [bx]
     mov dh, [bx]
     mov bh, 0
@@ -31,15 +67,17 @@ print_key ENDP
 
 print_key_end PROC
     ;function printing the end of a keyboard
+    mov bx, offset key_off
+    mov dh, [bx]
     mov bx, offset column
-    mov [bx], 0
+    mov [bx], dh
     mov dl, [bx]
     mov bx, offset row
     inc [bx]
     mov dh, [bx]
     mov bh, 0
     mov ah, 2
-    int 10h 
+    int 10h  ; move cursor to the right position 
     mov al, '-' ; character to print
     mov bx, offset color
     mov bl,[bx]; set the color (4 MSB -> background color, 4 LSB-> foreground color)
@@ -90,4 +128,5 @@ print_key_row ENDP
 
 color db 00001111b
 column db 0 ; must change value in code to display other touch
-row db 0
+row db 0 
+key_off db 0
