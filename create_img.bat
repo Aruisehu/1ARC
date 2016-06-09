5f034a9529b81391e35d1b98c9a9d4241fe287ad
robin4002 <robin4002@gmail.com>
2016-06-09 17:59:49 +0200

    update script

--- create_img.bat
+++ create_img.bat
@@ -1,2 +1,4 @@
-dd if=bin\bootstrap.bin of=img\final.img
-dd if=bin\main.bin of=img\final.img oflag=seek_bytes seek=512
+dd if=bin\bootstrap.bin of=img\final_en.img
+dd if=bin\bootstrap.bin of=img\final_fr.img
+dd if=bin\main_en.bin of=img\final_en.img oflag=seek_bytes seek=512
+dd if=bin\main_fr.bin of=img\final_fr.img oflag=seek_bytes seek=512
